3aeb287be05194395682112597cda2ff9b7fc3d1
Shaheen <joyce.shaheen@hp.com>
2018-03-04 18:47:08 -0600

    Please enter the co message for your changes. Lines starting

--- Outlier/Task1.r
+++
@@ -1,8 +0,0 @@
-library("RANN")
-
-nn <- function(data){
-  neighbor <- nn2(data[1:2], k=nrow(data), searchtype="radius", radius = 20)
-  data$ols <- 1/(rowSums(neighbor$nn.idx > 0))
-  dataset<-data
-  return(dataset)
-}
